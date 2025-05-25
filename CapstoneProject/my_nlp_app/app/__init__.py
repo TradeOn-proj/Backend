0e3eb56135c5b4170eb589824e5351c22ac88d2f
KimSeonMin00 <103050847+KimSeonMin00@users.noreply.github.com>
2025-05-25 22:27:21 +0900

    신규 블루프린트 추가

--- CapstoneProject/my_nlp_app/app/__init__.py
+++ CapstoneProject/my_nlp_app/app/__init__.py
@@ -10,19 +10,19 @@ def create_app():
     from app.routes.test import test_bp
     from app.routes.user import bp_user
     from app.routes.post import bp_post
-    from app.routes.review import bp_review
+    from app.routes.analytics import bp_analytics
     from app.routes.trade import bp_trade
     from app.routes.valuation import bp_valuation
-    from app.routes.userme import bp_userme
+    from app.routes.search import bp_search
     from app.routes.chat import bp_chat
 
     app.register_blueprint(test_bp)
     app.register_blueprint(bp_user)
     app.register_blueprint(bp_post)
-    app.register_blueprint(bp_review)
+    app.register_blueprint(bp_analytics)
     app.register_blueprint(bp_trade)
     app.register_blueprint(bp_valuation)
-    app.register_blueprint(bp_userme)
+    app.register_blueprint(bp_search)
     app.register_blueprint(bp_chat)
     
     socket_io.init_app(app)
