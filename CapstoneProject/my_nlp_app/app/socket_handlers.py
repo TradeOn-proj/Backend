a245b3a6583c1a458c72c7dcc950f51d5c1a1043
KimSeonMin00 <103050847+KimSeonMin00@users.noreply.github.com>
2025-05-26 22:15:52 +0900

    chat(프론트랑 합의 필요함)

--- CapstoneProject/my_nlp_app/app/socket_handlers.py
+++ CapstoneProject/my_nlp_app/app/socket_handlers.py
@@ -1,7 +1,22 @@
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+from flask import request
 
 def register_socketio_handlers(socketio : SocketIO):
+    @socketio.on('history')
+    def view_messagehistory(data):
+        roomid = data['roomid']
+
+        emit('history', to=request.sid)
+
     @socketio.on('message')
-    def handle_message(msg):
-        print('Message Received:', msg)
-        socketio.send(msg)
+    def handle_message(data):
+        username = data['username']
+        roomid = data['roomid']
+        msg = data['msg']
+
+        #db에 정보 저장
+
+        msg_data = {'user' : username, 'msg' : msg}
+
+        emit('message', msg_data, to=roomid)
+
